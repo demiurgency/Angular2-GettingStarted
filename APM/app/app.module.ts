e53d445febf680eaae35838cc5f53b9d8e499b5f
agreene <agreene@teksystems.com>
2017-03-01 16:16:27 -0500

    Configuring Routes

--- APM/app/app.module.ts
+++ APM/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
@@ -14,7 +15,14 @@ import { StarComponent } from './shared/star.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ], { useHash: true })
   ],
   declarations: [
     AppComponent,
@@ -26,4 +34,5 @@ import { StarComponent } from './shared/star.component';
   ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
