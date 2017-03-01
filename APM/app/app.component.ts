b2c66f13d1e2fe110cf7da8ccda168ba3881449a
agreene <agreene@teksystems.com>
2017-03-01 16:28:32 -0500

    Typing Routes to Actions

--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -4,9 +4,16 @@ import { ProductService } from './products/product.service';
 @Component({
     selector: 'pm-app',
     template: `
-    <div><h1>{{pageTitle}}</h1>
-        <pm-products></pm-products>
-        <pm-product-detail></pm-product-detail>
+    <div>
+        <nav class='navbar navbar-default'>
+            <div class='container-fluid'>
+                <a class='navbar-brand'>{{pageTitle}}</a>
+                <ul class='nav navbar-nav'>
+                    <li><a [routerLink]="['/welcome']">Home</a></li>
+                    <li><a [routerLink]="['/products']">ProductList</a></li>
+                </ul>
+            </div>
+        </nav>
     </div>
     `,
     providers: [ProductService]
