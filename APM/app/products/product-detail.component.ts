5205b02bbaa1aa688179fd5b28189d2c408dc52e
Greene <agreene@teksystems.com>
2017-03-02 12:14:11 -0500

    Passing Parameters to a Route

--- APM/app/products/product-detail.component.ts
+++ APM/app/products/product-detail.component.ts
@@ -1,14 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 import { IProduct } from "./product";
 
 @Component({
-    selector: 'pm-product-detail',
-    moduleId: module.id,
-    templateUrl: 'product-detail.component.html'
+    templateUrl: 'app/products/product-detail.component.html'
 })
-
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
     pageTitle: string = 'Product Detail';
     product: IProduct;
+
+    constructor(private _route: ActivatedRoute) {
+
+    }
+
+    ngOnInit(): void {
+        let id = +this._route.snapshot.params['id'];
+        this.pageTitle += `: ${id}`;
+    }
 }
