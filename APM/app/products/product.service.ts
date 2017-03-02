64e311a6ad1c4455793cd1a0841dc48160f00613
Greene <agreene@teksystems.com>
2017-03-02 13:15:03 -0500

    Finishing ProductDetail

--- APM/app/products/product.service.ts
+++ APM/app/products/product.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
+
 import { IProduct } from './product';
 
 @Injectable()
@@ -23,10 +26,8 @@ export class ProductService {
     }
 
     getProduct(id: number): Observable<IProduct> {
-        return this._http.get(this._productUrl)
-            .map((response: Response) => <IProduct>response.json()[0])
-            .do(data => console.log('All: ' + JSON.stringify(data)))
-            .catch(this.handleError);
+        return this.getProducts()
+            .map((products: IProduct[]) => products.find(p => p.productId === id));
     }
 
     private handleError(error: Response) {
